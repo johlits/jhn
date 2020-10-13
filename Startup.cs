7fe523239d64d68a575b83c1b11e0d724e516a06
Johan <johlits@palplanner.com>
2020-10-13 23:49:16 +0200

    add textfield

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Blazored.LocalStorage;
 
 namespace jhn
 {
@@ -7,6 +8,7 @@ namespace jhn
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddBlazoredLocalStorage();
         }
 
         public void Configure(IComponentsApplicationBuilder app)
